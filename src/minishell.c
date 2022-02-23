4f4ff2a304f07ae7d78cf57a57825075e25a5b5a
Сергей Тимошенков <serkoe2@gmail.com>
2022-02-23 19:48:25 +0300

    Add parsing commands

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: cchekov <cchekov@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 19:01:56 by cchekov           #+#    #+#             */
-/*   Updated: 2022/02/23 19:23:32 by cchekov          ###   ########.fr       */
+/*   Updated: 2022/02/23 19:47:33 by cchekov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,28 @@
 
 void shell(char *commandLine){
     char **commands;
+    int commandLen;
 
     commands = ft_split(commandLine, ' ');
+    commandLen = ft_strlen(commands[0]);
     // Parse command
-    ft_printf("%s",commands[0]);
+   
+    if (!ft_strncmp(commands[0], "echo", commandLen))
+        ft_printf("%s",commands[0]);
+    else if (!ft_strncmp(commands[0], "cd", commandLen))
+        ft_printf("%s",commands[0]);
+    else if (!ft_strncmp(commands[0], "pwd", commandLen))
+        ft_printf("%s",commands[0]);
+    else if (!ft_strncmp(commands[0], "export", commandLen))
+        ft_printf("%s",commands[0]);
+    else if (!ft_strncmp(commands[0], "unset", commandLen))
+        ft_printf("%s",commands[0]);
+    else if (!ft_strncmp(commands[0], "env", commandLen))
+        ft_printf("%s",commands[0]);
+    else if (!ft_strncmp(commands[0], "exit", commandLen))
+        ft_printf("%s",commands[0]);
+    else
+        ft_printf("Unknow command");
+    
+    return ;
 }
