ed9bc51d249f6c0ab8fc3d9c2754e9b0550ddb0b
Сергей Тимошенков <serkoe2@gmail.com>
2022-02-23 20:10:17 +0300

    add echo command handler

--- src/echo_handler.c
+++ src/echo_handler.c
@@ -1,21 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   echo_handler.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cchekov <cchekov@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/23 18:42:42 by cchekov           #+#    #+#             */
-/*   Updated: 2022/02/23 19:55:46 by cchekov          ###   ########.fr       */
+/*   Created: 2022/02/23 19:54:28 by cchekov           #+#    #+#             */
+/*   Updated: 2022/02/23 20:09:03 by cchekov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-# include "../libft/libft.h"
-# include "../ft_printf/ft_printf.h"
+#include "../headers/minishell.h"
 
-void	shell(char *commandLine);
-void	echo_handler(char **args);
+void echo_handler(char **args){
+    char	n_option;
+    int		i;
+    
+	i = 1;
+	n_option = 0;
+    if (!ft_strncmp(args[i], "-n", ft_strlen(args[0])))
+	{
+		n_option = 1;
+		i++;
+	}
 
-# endif
+    while (args[i])
+		ft_printf("%s", args[i++]);
+	if (!n_option)
+		ft_putchar('\n');
+}
--- src/minishell.c
+++ src/minishell.c
@@ -6,34 +6,34 @@
 /*   By: cchekov <cchekov@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 19:01:56 by cchekov           #+#    #+#             */
-/*   Updated: 2022/02/23 19:47:33 by cchekov          ###   ########.fr       */
+/*   Updated: 2022/02/23 19:57:21 by cchekov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
 
 void shell(char *commandLine){
-    char **commands;
+    char **command;
     int commandLen;
 
-    commands = ft_split(commandLine, ' ');
-    commandLen = ft_strlen(commands[0]);
+    command = ft_split(commandLine, ' ');
+    commandLen = ft_strlen(command[0]);
     // Parse command
    
-    if (!ft_strncmp(commands[0], "echo", commandLen))
-        ft_printf("%s",commands[0]);
-    else if (!ft_strncmp(commands[0], "cd", commandLen))
-        ft_printf("%s",commands[0]);
-    else if (!ft_strncmp(commands[0], "pwd", commandLen))
-        ft_printf("%s",commands[0]);
-    else if (!ft_strncmp(commands[0], "export", commandLen))
-        ft_printf("%s",commands[0]);
-    else if (!ft_strncmp(commands[0], "unset", commandLen))
-        ft_printf("%s",commands[0]);
-    else if (!ft_strncmp(commands[0], "env", commandLen))
-        ft_printf("%s",commands[0]);
-    else if (!ft_strncmp(commands[0], "exit", commandLen))
-        ft_printf("%s",commands[0]);
+    if (!ft_strncmp(command[0], "echo", commandLen))
+        echo_handler(command);
+    else if (!ft_strncmp(command[0], "cd", commandLen))
+        ft_printf("%s",command[0]);
+    else if (!ft_strncmp(command[0], "pwd", commandLen))
+        ft_printf("%s",command[0]);
+    else if (!ft_strncmp(command[0], "export", commandLen))
+        ft_printf("%s",command[0]);
+    else if (!ft_strncmp(command[0], "unset", commandLen))
+        ft_printf("%s",command[0]);
+    else if (!ft_strncmp(command[0], "env", commandLen))
+        ft_printf("%s",command[0]);
+    else if (!ft_strncmp(command[0], "exit", commandLen))
+        ft_printf("%s",command[0]);
     else
         ft_printf("Unknow command");
     
